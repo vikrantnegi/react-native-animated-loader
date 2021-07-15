6492e503dc3ef502a9f255bfed768f61e0a28ad4
Jack Hedaya <jackehedaya@gmail.com>
2021-07-15 18:19:16 -0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,12 +3,12 @@ export type AnimatedLoaderProps = {
    * Controls the visibility of the loader.
    * @defaultValue false
    */
-  visible: boolean
+  visible?: boolean
 
   /**
    * Changes the color of the overlay.
    */
-  overlayColor: string
+  overlayColor?: string
 
   /**
    * The source of animation. Can be referenced as a local asset by a string, or remotely with an object with a uri property, or it can be an actual JS object of an animation, obtained (for example) with something like  `require('../path/to/animation.json')`.
@@ -19,23 +19,23 @@ export type AnimatedLoaderProps = {
    * The speed the animation will progress.
    * @defaultValue 1
    */
-  speed: number
+  speed?: number
 
   /**
    * A boolean flag indicating whether or not the animation should loop.
    * @defaultValue true
    */
-  loop: boolean
+  loop?: boolean
 
   /**
    * Changes animation on show and hide loader's view.
    */
-  animationType: any
+  animationType?: any
 
   /**
    * The style to be applied to the Lottie.
    */
-  animationStyle: import('react-native').StyleProp<
+  animationStyle?: import('react-native').StyleProp<
     import('react-native').ViewStyle
   >
 }
